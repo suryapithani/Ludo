5ebe50c822bb0309e959fdfa7a7d8bb1fe4b02fb
suryapithani <105229063+suryapithani@users.noreply.github.com>
2022-05-14 15:01:00 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const secret = Math.random().toString(36).substring(7);
-
-module.exports = {
-    secret,
-    lobbyTimeout: 10000,
-    lobbyTimeoutCheckInterval: 5000,
-    port: 8080,
-    baseUrl: '/'       //For use with reverse proxies.
-};
